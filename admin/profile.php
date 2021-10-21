2f9fe9e5f5581e5e5c6a7331b4f1cc21a443a85d
Integer10 <76980794+Kantrawibawa10@users.noreply.github.com>
2021-10-21 20:47:57 +0800

    Add files via upload

---
+++ admin/profile.php
@@ -0,0 +1,220 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BOOMBEST.net</title>
+    <link rel="stylesheet" href="assets/css/pageadmin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+</head>
+<body>
+
+
+<header class="page-header">
+  <nav>
+      <a href="#0" aria-label="forecastr logo" class="logo">
+          <img src="assets/img/logo.png" alt="BOOMBEST">
+          <h2 style="font-size: 20px; margin-top: 10px; margin-left: -10px;">BOOMBEST</h2>
+      </a>
+
+      <ul id="btndahsboard" class="dashboard">
+          <li>
+              <a href="toko.php">
+                  <svg x="0px" y="0px" viewBox="0 0 306.773 306.773" style="enable-background:new 0 0 306.773 306.773;">
+                      <path d="M302.93,149.794c5.561-6.116,5.024-15.49-1.199-20.932L164.63,8.898
+                          c-6.223-5.442-16.2-5.328-22.292,0.257L4.771,135.258c-6.092,5.585-6.391,14.947-0.662,20.902l3.449,3.592
+                          c5.722,5.955,14.971,6.665,20.645,1.581l10.281-9.207v134.792c0,8.27,6.701,14.965,14.965,14.965h53.624
+                          c8.264,0,14.965-6.695,14.965-14.965v-94.3h68.398v94.3c-0.119,8.264,5.794,14.959,14.058,14.959h56.828
+                          c8.264,0,14.965-6.695,14.965-14.965V154.024c0,0,2.84,2.488,6.343,5.567c3.497,3.073,10.842,0.609,16.403-5.513L302.93,149.794z"/>
+                  </svg>
+                  <span>Dashboard</span>
+              </a>
+          </li>
+      </ul>
+
+      <ul  class="admin-menu" >
+          <li class="menu-heading">
+              <h3>Profile</h3>
+          </li>
+          <li id="btnpetugas">
+              <a href="profile.php">
+                  <svg  x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" >
+                      <path d="M256,0c-74.439,0-135,60.561-135,135s60.561,135,135,135s135-60.561,135-135S330.439,0,256,0z"/>
+                      <path d="M423.966,358.195C387.006,320.667,338.009,300,286,300h-60c-52.008,0-101.006,20.667-137.966,58.195
+                      C51.255,395.539,31,444.833,31,497c0,8.284,6.716,15,15,15h420c8.284,0,15-6.716,15-15
+                      C481,444.833,460.745,395.539,423.966,358.195z"/>
+                  </svg>
+                  <span>Profil Saya</span>
+              </a>
+          </li>
+          <li id="btnsiswa">
+              <a href="koin.php">
+                  <svg  x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" >
+                      <path d="M256,0c-74.439,0-135,60.561-135,135s60.561,135,135,135s135-60.561,135-135S330.439,0,256,0z"/>
+                      <path d="M423.966,358.195C387.006,320.667,338.009,300,286,300h-60c-52.008,0-101.006,20.667-137.966,58.195
+                      C51.255,395.539,31,444.833,31,497c0,8.284,6.716,15,15,15h420c8.284,0,15-6.716,15-15
+                      C481,444.833,460.745,395.539,423.966,358.195z"/>
+                  </svg>
+                  <span>Koin Saya</span>
+              </a>
+          </li>
+
+          <li class="menu-heading">
+              <h3>Toko</h3>
+          </li>
+          <li  id="btnbook">
+              <a href="#">
+                  <svg enable-background="new 0 0 513.124 513.124"  viewBox="0 0 513.124 513.124" >
+                      <path d="m503.685 29.49h-64.92c-5.213 0-9.439 4.326-9.439 9.663v27.841h83.799v-27.842c-.001-5.336-4.227-9.662-9.44-9.662z"/>
+                      <path d="m385.852 75.652h-64.919c-5.213 0-9.439 4.326-9.439 9.663v48.474h83.798v-48.474c-.001-5.337-4.227-9.663-9.44-9.663z"/>
+                      <path d="m429.325 101.832h83.799v338.833h-83.799z"/>
+                      <path d="m311.493 168.628h83.798v343.63h-83.798z"/>
+                      <path d="m429.325 475.504h83.799v36.754h-83.799z"/>
+                      <path d="m97.647 97.062h-88.208c-5.213 0-9.439 4.326-9.439 9.663v36.444h107.086v-36.444c0-5.337-4.226-9.663-9.439-9.663z"/>
+                      <path d="m0 178.007h107.086v334.251h-107.086z"/>
+                      <path d="m141.12 431.634h136.34v80.624h-136.34z"/>
+                      <path d="m141.12 116.749h136.34v280.047h-136.34z"/>
+                      <path d="m268.021.867h-117.462c-5.213 0-9.439 4.326-9.439 9.663v71.381h136.34v-71.382c0-5.336-4.226-9.662-9.439-9.662z"/>
+                  </svg>
+                  <span>Toko Saya</span>
+              </a>
+          </li>
+          <li id="btnsiswa">
+              <a href="tambahproduk.php">
+                  <svg enable-background="new 0 0 513.124 513.124"  viewBox="0 0 513.124 513.124" >
+                      <path d="m503.685 29.49h-64.92c-5.213 0-9.439 4.326-9.439 9.663v27.841h83.799v-27.842c-.001-5.336-4.227-9.662-9.44-9.662z"/>
+                      <path d="m385.852 75.652h-64.919c-5.213 0-9.439 4.326-9.439 9.663v48.474h83.798v-48.474c-.001-5.337-4.227-9.663-9.44-9.663z"/>
+                      <path d="m429.325 101.832h83.799v338.833h-83.799z"/>
+                      <path d="m311.493 168.628h83.798v343.63h-83.798z"/>
+                      <path d="m429.325 475.504h83.799v36.754h-83.799z"/>
+                      <path d="m97.647 97.062h-88.208c-5.213 0-9.439 4.326-9.439 9.663v36.444h107.086v-36.444c0-5.337-4.226-9.663-9.439-9.663z"/>
+                      <path d="m0 178.007h107.086v334.251h-107.086z"/>
+                      <path d="m141.12 431.634h136.34v80.624h-136.34z"/>
+                      <path d="m141.12 116.749h136.34v280.047h-136.34z"/>
+                      <path d="m268.021.867h-117.462c-5.213 0-9.439 4.326-9.439 9.663v71.381h136.34v-71.382c0-5.336-4.226-9.662-9.439-9.662z"/>
+                  </svg>
+                  <span>Tambah Produk</span>
+              </a>
+          </li>
+
+          <li>
+              <a href="logout.php">
+                  <svg x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" >
+
+                      <path d="M510.371,226.513c-1.088-2.603-2.645-4.971-4.629-6.955l-63.979-63.979c-8.341-8.32-21.824-8.32-30.165,0
+                                  c-8.341,8.341-8.341,21.845,0,30.165l27.584,27.584H320.013c-11.797,0-21.333,9.557-21.333,21.333s9.536,21.333,21.333,21.333
+                                  h119.168l-27.584,27.584c-8.341,8.341-8.341,21.845,0,30.165c4.16,4.181,9.621,6.251,15.083,6.251s10.923-2.069,15.083-6.251
+                                  l63.979-63.979c1.984-1.963,3.541-4.331,4.629-6.955C512.525,237.606,512.525,231.718,510.371,226.513z"/>
+                      <path d="M362.68,298.667c-11.797,0-21.333,9.557-21.333,21.333v106.667h-85.333V85.333c0-9.408-6.187-17.728-15.211-20.437
+                                  l-74.091-22.229h174.635v106.667c0,11.776,9.536,21.333,21.333,21.333s21.333-9.557,21.333-21.333v-128
+                                  C384.013,9.557,374.477,0,362.68,0H21.347c-0.768,0-1.451,0.32-2.197,0.405c-1.003,0.107-1.92,0.277-2.88,0.512
+                                  c-2.24,0.576-4.267,1.451-6.165,2.645c-0.469,0.299-1.045,0.32-1.493,0.661C8.44,4.352,8.376,4.587,8.205,4.715
+                                  C5.88,6.549,3.939,8.789,2.531,11.456c-0.299,0.576-0.363,1.195-0.597,1.792c-0.683,1.621-1.429,3.2-1.685,4.992
+                                  c-0.107,0.64,0.085,1.237,0.064,1.856c-0.021,0.427-0.299,0.811-0.299,1.237V448c0,10.176,7.189,18.923,17.152,20.907
+                                  l213.333,42.667c1.387,0.299,2.795,0.427,4.181,0.427c4.885,0,9.685-1.685,13.525-4.843c4.928-4.053,7.808-10.091,7.808-16.491
+                                  v-21.333H362.68c11.797,0,21.333-9.557,21.333-21.333V320C384.013,308.224,374.477,298.667,362.68,298.667z"/>
+                  </svg>
+
+                  <span>Logout</span>
+              </a>
+          </li>
+      </ul>
+  </nav>
+</header>
+<section class="page-content">
+  <section class="search-and-user">
+      <form action="" method="post">
+          <input type="" class="searchlah" style=" width: 100%;
+                                                  height: 50px;
+                                                  font-size: 1.5rem;
+                                                  padding-left: 15px;
+                                                  background: #fff;
+                                                  color: #333;
+                                                  border-radius: 4px;
+                                                  box-shadow: 0 0 10px -2px rgba(0, 0, 0, 0.075);" name="query" placeholder="Search...." autocomplete="off">
+          <div class="input-group-append">
+              <button type="submit" name="submit" class="input-group-text btn" aria-label="submit form">
+                  <svg aria-hidden="true">
+                      <path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/>
+                  </svg>
+              </button>
+          </div>
+      </form>
+
+      <a href="">
+          <div id="profilepetugas" class="admin-profile">
+              <span class="greeting">Hello</span>
+              <span class="greeting2"></span>
+              <div class="notifications">
+
+                  <svg  x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" >
+                      <path d="M256,0c-74.439,0-135,60.561-135,135s60.561,135,135,135s135-60.561,135-135S330.439,0,256,0z"/>
+                      <path d="M423.966,358.195C387.006,320.667,338.009,300,286,300h-60c-52.008,0-101.006,20.667-137.966,58.195
+                      C51.255,395.539,31,444.833,31,497c0,8.284,6.716,15,15,15h420c8.284,0,15-6.716,15-15
+                      C481,444.833,460.745,395.539,423.966,358.195z"/>
+                  </svg>
+
+              </div>
+          </div>
+      </a>
+
+
+  </section>
+
+    <section class="grid KANAN">
+
+      <section class="profile">
+         <div class="username dashboardprofile">
+            <div class="border-bg" style="display: flex; padding: 50px; background: #fff; border: 1px solid #707070;
+              width: 100%;
+              height: 30vh;
+              padding: 55px;
+              border-radius: 30px;">
+              <img src="assets/img/user.jpg" class="userprofile" alt="">
+              <div class="nama-profile">
+                <span>Profil</span>
+                <h2>Setya Novanto</h2>
+                <p>Member</p>
+              </div>
+            </div>
+         </div>
+      </section>
+
+    </section>
+
+    <section class="detail-user">
+       <div class="margin-user">
+         <div class="desc">
+            <h1>Kode Reveral</h1> <span style="margin-left: 60px;">: A001</span>
+         </div>
+         <div class="desc">
+            <h1>Nama</h1> <span style="margin-left: 127px;">: Setya Novanto</span>
+         </div>
+         <div class="desc">
+            <h1>Username</h1> <span style="margin-left: 89px;">: Novanto</span>
+         </div>
+         <div class="desc">
+            <h1>Email</h1> <span style="margin-left: 131px;">: novanto@gmail.com</span>
+         </div>
+         <div class="desc">
+            <h1>Gender</h1> <span style="margin-left: 114.5px;">: Laki - laki</span>
+         </div><div class="desc">
+            <h1>Alamat</h1> <span style="margin-left: 116.5px;">: Jl.Diponogoro No.27 Denpasar Utara</span>
+         </div>
+         <div class="desc">
+            <h1>Status</h1> <span style="margin-left: 125px;">: Member</span>
+         </div>
+
+         <div class="btn">
+           <a href="#">Edit</a>
+         </div>
+       </div>
+    </section>
+
+</section>
+
+
+</body>
+</html>
